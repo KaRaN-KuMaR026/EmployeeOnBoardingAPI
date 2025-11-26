76f61c2fda47c2cbf53b8d9166b5238cff9b6005
Karan Kumar <Karan7832kumar@gmail.com>
2025-11-26 15:23:37 +0530

    make employeeResponseDTO Serializable

--- src/main/java/com/karan/EmployeeOnboardingV2/dto/EmployeeResponseDTO.java
+++ src/main/java/com/karan/EmployeeOnboardingV2/dto/EmployeeResponseDTO.java
@@ -3,9 +3,11 @@ package com.karan.EmployeeOnboardingV2.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
 @Builder
-public class EmployeeResponseDTO {
+public class EmployeeResponseDTO implements Serializable {
     private Long id;
     private String name;
     private String email;
